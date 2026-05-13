2acf8e2db1630b29cba831ce407ba12b0456d36f
jorgev898 <jorgeisaacvergelgarcia@gmail.com>
2026-05-13 00:42:45 -0500

    Fix video URL to an official embeddable Jetpack Compose video

--- database/seeders/MvvmCourseSeeder.php
+++ database/seeders/MvvmCourseSeeder.php
@@ -112,7 +112,7 @@ class MvvmCourseSeeder extends Seeder
                         'type' => 'video',
                         'title' => 'Explicación Práctica',
                         'body' => 'Mira este video oficial de Android Developers para entender a fondo cómo funciona el Estado y la recomposición en Jetpack Compose.',
-                        'url' => 'https://www.youtube.com/embed/V-s4z8njlsQ' // Example video URL (can be changed)
+                        'url' => 'https://www.youtube.com/embed/bMcbHjU2CWs' // Oficial Jetpack Compose Basics
                     ],
                     [
                         'type' => 'concept',
